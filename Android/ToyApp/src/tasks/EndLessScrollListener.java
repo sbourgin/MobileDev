c60662dafcebf265eb610bf55dd4cacabdd26c39
Sylvain <sylvain@sylvain.fr>
2013-11-03 09:55:48 +0000

    Fetch data que si on est proche de la fin ou du début de la liste

--- Android/ToyApp/src/tasks/EndLessScrollListener.java
+++ Android/ToyApp/src/tasks/EndLessScrollListener.java
@@ -8,45 +8,50 @@ public class EndLessScrollListener implements OnScrollListener {
 	private CitiesListManager _citiesListManager = null;
 
 	private int _lastVisiblePosition = 0;
-	
-	
+
 	public EndLessScrollListener(CitiesListManager parCitiesListManager) {
 		_citiesListManager = parCitiesListManager;
-		
+
 	}
-	
-	
+
 	@Override
 	public void onScroll(AbsListView arg0, int arg1, int arg2, int arg3) {
-			
+
 	}
 
 	@Override
 	public void onScrollStateChanged(AbsListView parListView, int parScrollState) {
-		
-		@SuppressWarnings("unused")
-		int count = parListView.getCount();
+
+		boolean isUpdateNeeded = false; // TODO tester si le code est bon
+
+		int locItemsCount = parListView.getCount();
+
 		boolean isScrollingDown;
-		
+
 		int locNewPosition = parListView.getLastVisiblePosition();
-		
-		if(_lastVisiblePosition < locNewPosition) {
+
+		if (_lastVisiblePosition < locNewPosition) {
+
+			int locLastItemVisible = parListView.getLastVisiblePosition();
+			if ((locLastItemVisible + 20) >= locItemsCount) {
+				isUpdateNeeded = true;
+			}
+
 			isScrollingDown = true;
 		} else {
+			int locFirstItemVisible = parListView.getFirstVisiblePosition();
+			if (locFirstItemVisible - 20 <= 0) {
+				isUpdateNeeded = true;
+			}
 			isScrollingDown = false;
 		}
-		
-		_citiesListManager.updateCitiesList(isScrollingDown);
-		
-		/*
-		Toast locToast = Toast.makeText(_contextParent,
-				locMessage, Toast.LENGTH_SHORT);
-		locToast.show();
-		*/
+
+		if (isUpdateNeeded) {
+			_citiesListManager.updateCitiesList(isScrollingDown);
+		}
+
 		_lastVisiblePosition = locNewPosition;
-		
-		
+
 	}
-	
 
 }
