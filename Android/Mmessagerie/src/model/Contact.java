b35443c76edd9e7dce2bb2ede08198b1db99c726
Sylvain <sylvain@sylvain.fr>
2013-11-22 15:11:16 +0000

    Ajout model Contact, Message

---
+++ Android/Mmessagerie/src/model/Contact.java
@@ -0,0 +1,25 @@
+package model;
+
+import interfaces.Displayable;
+
+public class Contact implements Displayable{
+
+	@Override
+	public String getNameToDisplay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSubtitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getIdOfItem() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Android/Mmessagerie/src/model/Message.java
@@ -0,0 +1,28 @@
+package model;
+
+import interfaces.Displayable;
+
+public class Message implements Displayable{
+
+	
+	
+	
+	@Override
+	public String getNameToDisplay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSubtitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getIdOfItem() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
