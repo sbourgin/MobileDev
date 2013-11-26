051de763d62a23e2723799b4feaf3eeab2219381
vbourgeois <vincent.bourgeois@insa-lyon.fr>
2013-11-26 22:03:59 +0100

    Create project with split view

---
+++ Windows8/MessagingApp/MessagingApp/pages/items/items.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    "use strict";
+
+    var ui = WinJS.UI;
+
+    ui.Pages.define("/pages/items/items.html", {
+        // This function is called to initialize the page.
+        init: function (element, options) {
+            this.itemInvoked = ui.eventHandler(this._itemInvoked.bind(this));
+        },
+
+        // This function is called whenever a user navigates to this page.
+        ready: function (element, options) {
+        },
+
+        // This function updates the page layout in response to layout changes.
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in layout.
+        },
+
+        _itemInvoked: function (args) {
+            var groupKey = Data.groups.getAt(args.detail.itemIndex).key;
+            WinJS.Navigation.navigate("/pages/split/split.html", { groupKey: groupKey });
+        }
+    });
+})();
