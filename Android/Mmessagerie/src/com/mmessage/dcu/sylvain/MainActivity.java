b47014dd57cb5c87e2678cdd62bbfb52980696f8
Sylvain <sylvain@sylvain.fr>
2013-12-28 13:53:06 +0100

    Premier Layout authentification

--- Android/Mmessagerie/src/com/mmessage/dcu/sylvain/MainActivity.java
+++ Android/Mmessagerie/src/com/mmessage/dcu/sylvain/MainActivity.java
@@ -1,15 +1,38 @@
 package com.mmessage.dcu.sylvain;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
 
+	private RelativeLayout _layout = null;
+	private EditText _login = null;
+	private EditText _password = null;
+	private Button _createAccount = null;
+	private Button _signIn = null;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		
+		_layout = (RelativeLayout) RelativeLayout.inflate(this,
+				R.layout.activity_main, null);
+		
+		_login =  (EditText) _layout.findViewById(R.id.mainUserName);
+		_password = (EditText) _layout.findViewById(R.id.mainPassword);
+		_createAccount = (Button) _layout.findViewById(R.id.mainCreateAccount);
+		_signIn = (Button) _layout.findViewById(R.id.mainSignIn);
+		
+		_createAccount.setText("Create an account");
+		_signIn.setText("Sign In");
+		
+		setContentView(_layout);
 	}
 
 	@Override
